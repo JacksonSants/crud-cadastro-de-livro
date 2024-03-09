0ff38b406ad911a04923e55ec5e974ef2e43f7a0
JacksonSants <jacksondiego2002@gmail.com>
2024-03-08 20:04:05 -0500

    Atualização de arquivos

---
+++ js/hidden.js
@@ -0,0 +1,15 @@
+function criarConta() {
+    let login = document.getElementById('login');
+    let register = document.getElementById('register');
+
+    login.classList.add('hidden');
+    register.classList.remove('hidden');
+}
+
+function entrarConta() {
+    let login = document.getElementById('login');
+    let register = document.getElementById('register');
+
+    login.classList.remove('hidden');
+    register.classList.add('hidden');
+}
